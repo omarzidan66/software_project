85c7d698d8ceac8265ada727f5ed4cc8d94a8e36
omarzidan66 <126962673+omarzidan66@users.noreply.github.com>
2025-04-04 23:03:37 +0300

    Add files via upload

---
+++ Sofware_04042025/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // important: true,
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        Iris: "#5D3FD3",
+      },
+    },
+    fontFamily: {
+      body: ["inter"],
+      sans: ["Open Sans", "Noto Kufi Arabic", "Arial", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
